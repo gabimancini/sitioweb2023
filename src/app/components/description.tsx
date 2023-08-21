94fda8f4fcbb0283a11a30f4f5aaa39a4eff08e8
gabimancini <gabimancinir@gmail.com>
2023-08-21 19:44:16 -0400

    Fix font and alignment in mission section

--- src/app/components/description.tsx
+++ src/app/components/description.tsx
@@ -4,7 +4,7 @@ type DescriptionProps = {
 const Description = (props: DescriptionProps)=>{
     const{text}=props
     return(
-        <p className="text-[12px] mb-[40px] md:text-[14px] lg:text-[18px]">{text}</p>
+        <p className="text-[14px] mb-[40px] lg:text-[18px]">{text}</p>
     )
 }
 export default Description;
