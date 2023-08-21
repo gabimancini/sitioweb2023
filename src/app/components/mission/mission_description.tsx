e03c47b6fc5c6a5039fad82b7a8ac9cf8bfb79a0
gabimancini <gabimancinir@gmail.com>
2023-08-21 15:04:32 -0400

    Change mission section names

---
+++ src/app/components/mission/mission_description.tsx
@@ -0,0 +1,9 @@
+import { Azeret_Mono } from 'next/font/google';
+import Description from '../description';
+const DescriptionMission = ()=>{
+    let missionDesc="The mission of this company is to provide our clients with a quality service. We consider that each website or e-commerce store represents an important aspect of each business. We are glad to be part in the process of telling the world about the projects of each person";
+    return(
+        <Description text={missionDesc} />
+    )
+}
+export default DescriptionMission;
---
+++ src/app/components/mission/mission_subtitle.tsx
@@ -0,0 +1,7 @@
+import SubTitleh2 from '../subtitles';
+const Subtitle = ()=>{
+    return(
+        <SubTitleh2 text="hola" />
+    )
+}
+export default Subtitle
