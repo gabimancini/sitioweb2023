455e6ccdadf4d23a87c5252dd5de2d46446c0c19
gabimancini <gabimancinir@gmail.com>
2023-08-21 22:34:01 -0400

    Try to fix changes on next and run gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0596407fb54a0c22"; }
+/******/ 		__webpack_require__.h = function() { return "69e91c63741cb491"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b80530ef5e4a174f"; }
+/******/ 	__webpack_require__.h = function() { return "4170fe25f824520e"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
