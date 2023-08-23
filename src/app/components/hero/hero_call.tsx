fdc643492de88af6a6574f6982fcac13244b539a
gabimancini <gabimancinir@gmail.com>
2023-08-22 20:55:58 -0400

    Add margin to boton in hero section

--- src/app/components/hero/hero_call.tsx
+++ src/app/components/hero/hero_call.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const HeroButton = ()=>{
     return(
         <Link href="/contact" className="text-white bg-btn_call rounded-btn_call px-6 md:px-8 py-2 md:py-3 inline-block text-xs md:text-lg">
-        Get in touch
+        Get started
       </Link>
     )
 }
--- src/app/components/hero/hero_paragraph.tsx
+++ src/app/components/hero/hero_paragraph.tsx
@@ -1,7 +1,7 @@
 const HeroParagraph = ()=>{
     const text = "In an interconnected world, expanding your business's horizons has never been more essential. Our expertly crafted user experiences ensure that your brand's message transcends linguistic and cultural barriers, resonating effortlessly with audiences worldwide."
     return(
-      <p className="text-white max-w-2xl text-sm md:text-lg mb-4 md:mb-8 md:mb-[49px] tracking-[0.9px]">{text}</p>
+      <p className="text-white max-w-2xl text-sm md:text-lg mb-8 md:mb-8 md:mb-[49px] tracking-[0.9px]">{text}</p>
     )
 }
 export default HeroParagraph;
